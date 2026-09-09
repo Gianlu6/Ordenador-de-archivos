9eef6057a725f5cf71aa65c031c114ca00967722
Gianlu6 <gianlucanicotra06@gmail.com>
2026-09-09 14:47:32 -0300

    Update fileSorter.py
    Ahora podes decirle el nombre de tu carpeta y va a ordenarla de una

--- fileSorter.py
+++ fileSorter.py
@@ -8,21 +8,27 @@ CATEGORIAS = {
     ".pdf": "Documentos", ".docx": "Documentos", ".txt": "Documentos",
     ".mp3": "Musica", ".wav": "Musica",
     ".csv": "Datos", ".json": "Datos", ".xls": "Datos",
-    ".mp4": "Video", ".avi": "Video", ".mov": "Video",
-    ".exe": "Ejecutables", ".bat": "Ejecutables"
-}
+    ".mp4": "Video", ".avi": "Video", ".mov": "Video"
+
+    }
 
 def main():
-    #Al pegar el path aca, asegurate de ponerle "" y cambiar la barra diagonal a /
-    path = #your path
-
-    if os.path.exists(#your path):
+    usuario = os.path.expanduser("~")
+    
+    carpeta = input("¿Que directorio queres ordenar?: ").strip()
+    
+    for dirs, subdirs, files in os.walk(usuario, topdown=True, onerror=None):
+        if carpeta in subdirs:
+            path = os.path.join(dirs, carpeta)
+    
+    print(path)
+    
+    if os.path.exists(path):
         try:            
             for file in os.listdir(path):
                 ruta_origen = os.path.join(path, file)
                 _, extension = os.path.splitext(file)        
                 extension = extension.lower()
-                print(f"Archivo: {file}, extension: {extension}")
                 create_dirs(ruta_origen, extension, path)
                 
         except Exception as e:
@@ -31,7 +37,7 @@ def main():
         print("El path no existe")
 
 def create_dirs(file, file_type, base_path):
-    tipos = ['imagenes', 'documentos', 'musica', 'datos']
+    tipos = ['imagenes', 'documentos', 'musica', 'datos', 'video']
     for type, folder in CATEGORIAS.items():
         if type == file_type:
             for tipo in tipos:
@@ -43,6 +49,6 @@ def create_dirs(file, file_type, base_path):
                     ruta_completa = os.path.join(carpeta_destino, os.path.basename(file))
 
                     os.rename(file, ruta_completa)
-
+                
 if __name__ == "__main__":
     main()
